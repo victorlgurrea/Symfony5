ee6ca3b2bae0760febd09856a9951242f5590e22
victorlgurrea <victorlgurre@gmail.com>
2020-12-01 18:46:43 +0100

    primer commit symfony 5

---
+++ src/Controller/CategoriaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoriaController extends AbstractController
+{
+    /**
+     * @Route("/categoria", name="app_categoria")
+     */
+    public function index(): Response
+    {
+        return $this->render('categoria/index.html.twig', [
+            'controller_name' => 'CategoriaController',
+        ]);
+    }
+}
